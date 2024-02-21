a928fcafdad617cb9b6c4c1967e8128f83078eaf
Jaithecomder <jaib2910@gmail.com>
2024-02-21 13:29:04 +0530

    data prep and ann

--- ANN.py
+++ ANN.py
@@ -1,13 +1,49 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import torch
 
 class ANN(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size, output_size, hidden_size):
         super(ANN, self).__init__()
-        self.fc1 = nn.Linear(10, 5)
-        self.fc2 = nn.Linear(5, 2)
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
+        x = self.fc1(x)
+        x = F.relu(x)
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return F.softmax(x, dim=1)
+    
+def dataPrep(trainX, trainY, contextSize, pad):
+    X = []
+    Y = []
+    for s in range(len(trainX)):
+        sentence = trainX[s]
+        for i in range(len(sentence)):
+            x = []
+            y = []
+            for j in range(i - contextSize, i + contextSize + 1):
+                if j < 0 or j >= len(sentence):
+                    x.append(pad)
+                else:
+                    x.append(sentence[j])
+            X.append(x)
+            Y.append(trainY[s][i])
+    return X, Y
+    
+def trainANN(trainX , trainY, pad, contextSize=2, device='cpu'):
+    trainX, trainY = dataPrep(trainX, trainY, contextSize, pad)
+
+    trainX = torch.tensor(trainX, dtype=torch.float)
+    trainX = torch.flatten(trainX, start_dim=1).to(device)
+    trainY = torch.tensor(trainY, dtype=torch.float).to(device)
+
+    print(trainX.shape, trainY.shape)
+
+    model = ANN(len(trainX[0]), len(trainY[0]), 100).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # for epoch in range(100):
--- pos_tagger.py
+++ pos_tagger.py
@@ -1,10 +1,57 @@
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torch
 import conllu
+import torch
 
-trainSet = conllu.parse_incr(open("./data/UD_English-Atis/en_atis-ud-train.conllu", "r", encoding="utf-8"))
-testSet = conllu.parse_incr(open("./data/UD_English-Atis/en_atis-ud-test.conllu", "r", encoding="utf-8"))
+from ANN import trainANN
 
-print(trainSet)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+with open("./data/UD_English-Atis/en_atis-ud-train.conllu", "r", encoding="utf-8") as f:
+    trainSet = conllu.parse(f.read())
+with open("./data/UD_English-Atis/en_atis-ud-dev.conllu", "r", encoding="utf-8") as f:
+    devSet = conllu.parse(f.read())
+with open("./data/UD_English-Atis/en_atis-ud-test.conllu", "r", encoding="utf-8") as f:
+    testSet = conllu.parse(f.read())
+
+vocab = set()
+pos = ['ADJ', 'ADP', 'ADV', 'AUX', 'CCONJ', 'DET', 'INTJ', 'NOUN', 'NUM', 'PART', 'PRON', 'PROPN', 'PUNCT', 'SCONJ', 'SYM', 'VERB', 'X']
+
+for sentence in trainSet:
+    for word in sentence:
+        vocab.add(word['form'])
+
+vocab.add('<OOV>')
+vocab.add('<PAD>')
+
+def oneHotEncode(sentenceSet, vocab, pos):
+    X = []
+    Y = []
+
+    for sentence in sentenceSet:
+        x = []
+        y = []
+        for word in sentence:
+            wx = [0] * len(vocab)
+            wy = [0] * len(pos)
+            if word['form'] in vocab:
+                wx[list(vocab).index(word['form'])] = 1
+            else:
+                wx[list(vocab).index('<OOV>')] = 1
+            wy[list(pos).index(word['upos'])] = 1
+            x.append(wx)
+            y.append(wy)
+        X.append(x)
+        Y.append(y)
+
+    return X, Y
+    
+
+trainX, trainY = oneHotEncode(trainSet, vocab, pos)
+devX, devY = oneHotEncode(devSet, vocab, pos)
+testX, testY = oneHotEncode(testSet, vocab, pos)
+
+context = 2
+
+pad = [0] * len(vocab)
+pad[list(vocab).index('<PAD>')] = 1
+model = trainANN(trainX, trainY, pad, context, device)
+
